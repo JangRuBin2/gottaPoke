78a1a2219f9539f5a170801dd07eb09a4ef4d286
rubingJang <wkdfnqls2465@gmail.com>
2024-12-31 15:58:52 +0900

    feat : 포켓몬 카드 css 정렬

--- gotta-poke-app/app/gotta/_components/PoketmonCard.tsx
+++ gotta-poke-app/app/gotta/_components/PoketmonCard.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { CN } from "@/app/_utils/CN";
+import Image from "next/image";
 import { useState } from "react";
 import styles from "./PoketmonCard.module.css";
 const PoketmonCard = ({
@@ -17,7 +18,24 @@ const PoketmonCard = ({
       className={CN([styles.cardContainer, !isOpen ? styles.disabled : ""])}
       onClick={handleOpen}
     >
-      {isOpen ? <div>{poke.name}</div> : <div></div>}
+      {isOpen ? (
+        <>
+          <div className={styles.title}>
+            <span>{poke.name}</span>
+          </div>
+          <div className={styles.container}>
+            <span>
+              <Image
+                src={poke.sprites.front_default}
+                layout="intrinsic"
+                alt={`gottaPoke_${poke.name}`}
+              ></Image>
+            </span>
+          </div>
+        </>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
