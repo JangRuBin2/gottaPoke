bfa11e86c7efdd6bc9a0b60cd980ccea0ceb24b9
JangRuBin2 <wkdfnqls2465@gmail.com>
2025-01-01 10:46:17 +0900

    fix: 한글 설명글 없으면 영문 노출되도록 수정, 카드 크기 조정

--- gotta-poke-app/app/gotta/_components/PokeDetailInfo.tsx
+++ gotta-poke-app/app/gotta/_components/PokeDetailInfo.tsx
@@ -18,31 +18,35 @@ const PokeDetailInfo = ({
   const [showInfo, isShowInfo] = useState<boolean>(false);
   return (
     <div className={styles.wrapper} onClick={() => isShowInfo((p) => !p)}>
-      <div className={styles.title}>
-        <span>{getKoreanInfo(poke.genera)}</span>
-        <h4>{getKoreanName(poke.names)}</h4>
-        <ExitIcon onClick={() => setIsDetailOpen(false)} />
-      </div>
-      {showInfo && (
-        <div className={styles.info}>
-          <span>{getKoeanFlavor(poke.flavor_text_entries)}</span>
+      <div className={styles.container}>
+        <div className={styles.title}>
+          <div className={styles.name}>
+            <span>{getKoreanInfo(poke.genera)}</span>
+            <h4>{getKoreanName(poke.names)}</h4>
+          </div>
+          <ExitIcon onClick={() => setIsDetailOpen(false)} />
         </div>
-      )}
+        {showInfo && (
+          <div className={styles.info}>
+            <span>{getKoeanFlavor(poke.flavor_text_entries)}</span>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
 const getKoreanInfo = (data: Genera[]) => {
-  const result = data.find((d) => d.language.name === "ko");
+  const result = data.find((d) => d.language.name === "ko" || "en");
   if (!result) return "";
   return result.genus;
 };
 const getKoreanName = (data: Names[]) => {
-  const result = data.find((d) => d.language.name === "ko");
+  const result = data.find((d) => d.language.name === "ko" || "en");
   if (!result) return "";
   return result.name;
 };
 const getKoeanFlavor = (data: FlavorText[]) => {
-  const result = data.find((d) => d.language.name === "ko");
+  const result = data.find((d) => d.language.name === "ko" || "en");
   if (!result) return "";
   return result.flavor_text;
 };
