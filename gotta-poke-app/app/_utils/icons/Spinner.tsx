9e6c239caae06dd154a933d21711af4d4203310c
JangRuBin2 <wkdfnqls2465@gmail.com>
2025-01-01 16:20:26 +0900

    fix : 스피너 크기, 색상 조정

--- gotta-poke-app/app/_utils/icons/Spinner.tsx
+++ gotta-poke-app/app/_utils/icons/Spinner.tsx
@@ -1,7 +1,7 @@
 import { BounceLoader } from "react-spinners";
 
 const Spinner = ({ loading }: { loading: boolean }) => {
-  return <BounceLoader size={50} color={"#d2ab36"} loading={loading} />;
+  return <BounceLoader size={180} color={"#a5e67a"} loading={loading} />;
 };
 
 export default Spinner;
