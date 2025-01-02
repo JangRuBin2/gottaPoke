f4b2c001b1a650138b91dc389ce088c521a7924c
JangRuBin2 <wkdfnqls2465@gmail.com>
2025-01-02 18:50:17 +0900

    feat: 사기모드 링크 추가

--- gotta-poke-app/app/lover/[love]/page.tsx
+++ gotta-poke-app/app/lover/[love]/page.tsx
@@ -1,5 +1,6 @@
 import loverLogo from "@/app/_utils/images/loverLogo.png.png";
 import Image from "next/image";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 import styles from "./LoverPage.module.css";
 const LoverPage = async ({ params }: { params: { love: string } }) => {
@@ -15,6 +16,7 @@ const LoverPage = async ({ params }: { params: { love: string } }) => {
           style={{ objectFit: "contain" }}
         />
       </span>
+      <Link href={"/gotta?love=forever"}>{"사기 모드 시작하기"}</Link>
     </div>
   );
 };
