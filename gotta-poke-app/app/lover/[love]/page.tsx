ed74bdc2d27bb119ae70244549ac635b84f6a490
JangRuBin2 <wkdfnqls2465@gmail.com>
2025-01-02 20:59:08 +0900

    fix : 빌드 오류수정 => 클라이언트, 서버 컴포넌트 분리(use client사용해도 클라이언트 컴포넌트로 인식못함)

--- gotta-poke-app/app/lover/[love]/page.tsx
+++ gotta-poke-app/app/lover/[love]/page.tsx
@@ -1,11 +1,8 @@
 import loverLogo from "@/app/_utils/images/loverLogo.png.png";
 import Image from "next/image";
 import Link from "next/link";
-import { redirect } from "next/navigation";
 import styles from "./LoverPage.module.css";
-const LoverPage = async ({ params }: { params?: { love?: string } }) => {
-  const { love } = params;
-  if (!love) redirect("/");
+const LoverPage = () => {
   return (
     <div className={styles.container}>
       <span>
