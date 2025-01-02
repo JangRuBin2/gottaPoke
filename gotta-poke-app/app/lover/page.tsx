a4c810bbed4f4883d8919f7598e6386442897ebe
rubingJang <wkdfnqls2465@gmail.com>
2025-01-02 14:50:04 +0900

    feat: 성호정 전용 페이지 제작

---
+++ gotta-poke-app/app/lover/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import styles from "./LoverPage.module.css";
+const LoverPage = () => {
+  const router = useRouter();
+  const answer = "장루빈최고";
+  const [inputValue, setInputValue] = useState<string>("");
+  const handleInput = (val: string) => {
+    setInputValue(val);
+  };
+  const getAnswer = () => {
+    if (inputValue !== answer) return alert("잘못된 접근입니다.");
+    alert("호정아 생일축하해~ 사랑해");
+    router.push("/gotta?love=forever");
+  };
+  return (
+    <div className={styles.container}>
+      <div>
+        <span>{"당신은 성호정입니까?"}</span>
+        <input
+          type="text"
+          onChange={(e) => handleInput(e.target.value)}
+          value={inputValue}
+        />
+        <button disabled={!inputValue.length} onClick={getAnswer}>
+          {"제출"}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LoverPage;
--- gotta-poke-app/app/page.tsx
+++ gotta-poke-app/app/page.tsx
@@ -1,6 +1,7 @@
 import mainImage from "@/app/_utils/images/pngwing.png";
 import Image from "next/image";
 import Link from "next/link";
+import LoverIcon from "./_utils/icons/LoverIcon";
 import styles from "./mainPage.module.css";
 
 const MainPage = () => {
@@ -16,6 +17,9 @@ const MainPage = () => {
           />
         </span>
         <Link href={"/gotta"}>{"시작하려면 화면을 눌러주세요."}</Link>
+        <Link href={"/lover"}>
+          <LoverIcon className={styles.lover} />
+        </Link>
       </div>
     </>
   );
